9a74b7a880ee5930439d79378196c0eda4d23a58
Azadarx <syedazadarhussayn@gmail.com>
2025-04-25 05:04:32 +0530

    success page update

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ServicesPage from './Frontend-components/ServicesPage';
-import SignInPage from './Frontend-components/SignInPage';
-import SignUpPage from './Frontend-components/SignUpPage';
+import HeroPage from './Frontend-components/HeroPage';
+import ServiceCard from './Frontend-components/ServiceCard';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
 import PaymentPage from './Frontend-components/PaymentPage';
 import SuccessPage from './Frontend-components/SuccessPage';
 import ProtectedRoute from './Frontend-components/ProtectedRoute';
-import HeroPage from './Frontend-components/HeroPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HeroPage />} />
-      <Route path="/services" element={<ServicesPage />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/services" element={<ServiceCard />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/payment" element={<PaymentPage />} />
       <Route path="/success" element={<SuccessPage />} />
       
